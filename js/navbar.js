57c54fca602872e03f1f0106121063f6d0f4c1bf
skimrme <sperdemaj146@gmail.com>
2026-07-29 14:59:34 +0800

    Pr-2026-07-29-Add-More
    添加Vue.js
    添加Navbar.js
    修改8个页面的Navbar,确认没问题可以展示

---
+++ js/navbar.js
@@ -0,0 +1,34 @@
+// 读取json文件
+fetch('../data/navbar.json')
+.then(response => {
+    return response.json();
+})
+.then(data => {
+    const json_home = data[0];
+    const json_articles = data[1];
+    const json_diary = data[2];
+    const json_gallery = data[3];
+    const json_video = data[4];
+    const json_game = data[5];
+    const json_memories = data[6];
+    const json_rumors = data[7];
+    const json_about = data[8];
+
+    // Vue 循环
+    new Vue({
+        el: '#navbar',
+        data: {
+            lists: [
+                json_home,
+                json_articles,
+                json_diary,
+                json_gallery,
+                json_video,
+                json_game,
+                json_memories,
+                json_rumors,
+                json_about
+            ]
+        }
+    })
+});
